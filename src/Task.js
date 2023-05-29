c29ecb75c5d6df5b9136ab11d7b3fd627d53a7ef
xsmobi <eckard.ritter@googlemail.com2>
2023-05-29 21:24:44 +0200

    25.05.2023

--- src/Task.js
+++ src/Task.js
@@ -1,22 +1,25 @@
-//import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 // <MathJaxContext>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJaxContext>
 // funktioniert!
 const style={
-    taskheader:`text-1xl font-bold text-center text-gray-800 p-2`,
+    taskheader:`text-1xl font-bold text-center text-gray-800 p-0`,
     tasktext:`text-neutral-800 dark:text-neutral-400`
 }
+
 
 function Task({ task, showHelp, showResult, showExplainer, toggleShowHelp }) {
     const { text, help, answer, explainer } = task;
     return (
+    <MathJaxContext><MathJax>
     <div className="Task">
         <h3 className={style.taskheader}>{text}</h3>
-        
         {showHelp && <div className={style.taskheader} dangerouslySetInnerHTML={{ __html: help }} />}
         {showResult && <h3 className={style.taskheader}>{answer}</h3>}
         {showExplainer && <div  className={style.tasktext} dangerouslySetInnerHTML={{ __html: explainer }} />}
     </div>
+    </MathJax></MathJaxContext>
     );
     }
     
+
     export default Task;
--- src/components/frac.js
+++ src/components/frac.js
@@ -1,29 +1,25 @@
-import React from "react";
-import { MathJax, MathJaxContext } from "better-react-mathjax";
-
-/*
-export default function App() {
-  return (
-      <MathJaxContext>
-            <h2>Basic MathJax example with Latex</h2>
-            <MathJax>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJax>
-      </MathJaxContext>
-  );
-}
-*/
-
 function frac(min, max){
     let aufgabe, loesung, help, num, den
-    num = getRandomInt2(20, 2);
-    den = getRandomInt2(40, 2);
-    aufgabe = num/den;
-    <MathJaxContext>
-        aufgabe = <MathJax>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJax>;
-        console.log(aufgabe)
-    </MathJaxContext>
-    // aufgabe = "...aufgabe"
-    loesung = ""
-    help = ""
+    const divisors = [2,3,5,7,9]
+    const randomDivisor = divisors[Math.floor(Math.random() * divisors.length)];
+    num = getRandomInt2(20, randomDivisor);
+    den = getRandomInt2(40, randomDivisor);
+    //aufgabe = num/den;
+    aufgabe = `\\[\\frac{${num}}{${den}}\\]`;
+    loesung = getlowestfraction(num/den);
+    let numdiv = findDivisors(num).toString()
+    //numdiv = numdiv.substring(0,numdiv.length-1);
+    let dendiv = findDivisors(den).toString();
+    //dendiv = dendiv.substring(0,dendiv.length-1);
+    let numprime = primeFactors(num).toString()
+    //numprime = numprime.substring(0,numprime.length-1);
+    let denprime = primeFactors(den).toString();
+    //denprime = denprime.substring(0,denprime.length-1);
+
+
+
+    help = `Der Zähler ist das zusammengesetzt aus den möglichen Teilern: ${numdiv}, der Nenner aus ${dendiv}. Der Zähler ist das Produkt ${numprime}, der Nenner aus ${denprime}. `
+    
     return [aufgabe, loesung, help];
 }
 export default frac;
@@ -35,14 +31,12 @@ function getRandomInt2(max, n) { // n = Teiler = 2, 3, 5, 7
 function getlowestfraction(x0) {
     var eps = 1.0E-15;
     var h, h1, h2, k, k1, k2, a, x;
-
     x = x0;
     a = Math.floor(x);
     h1 = 1;
     k1 = 0;
     h = a;
     k = 1;
-
     while (x-a > eps*k*k) {
         x = 1/(x-a);
         a = Math.floor(x);
@@ -51,6 +45,36 @@ function getlowestfraction(x0) {
         h = h2 + a*h1;
         k = k2 + a*k1;
     }
-
-    return h + "/" + k;
+    // return h + "/" + k;
+   return `\\[\\frac{${h}}{${k}}\\]`; 
 }
+
+function findDivisors(integer) { //https://dev.to/cesar__dlr/32-find-the-divisors-codewars-kata-7-kyu-5f7n
+    let r = []
+    for(let i = 2; i<integer; i++){
+      if(integer%i === 0) r.push(i)
+    }
+    let res = r.length !== 0 ? r : `${integer} is prime`
+    return res
+  }
+
+  
+  function primeFactors(num) {
+    function is_prime(num) {
+      for (let i = 2; i <= Math.sqrt(num); i++)
+      {
+        if (num % i === 0) return false;
+      }
+      return true;
+    }
+    const result = [];
+    for (let i = 2; i <= num; i++)
+    {
+      while (is_prime(i) && num % i === 0) 
+      {
+        if (!result.includes(i)) result.push(i);
+        num /= i;
+      }
+    }
+    return result;
+  }
