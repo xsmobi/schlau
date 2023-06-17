d16957dfbf80d9df3525c97b8bc18ecea35ead18
xsmobi <eckard.ritter@googlemail.com2>
2023-06-17 11:59:07 +0200

    Erstmalig PWA

--- src/Task.js
+++ src/Task.js
@@ -9,35 +9,21 @@ const style={
 function Task({ task, showHelp, showResult, showExplainer }) {
     const { text, help, answer, explainer, headerclass } = task;
     const headerClassName = headerclass ? style[headerclass] : style.taskheader;
-    //console.log(headerclass)
-    //<h3 className={style.taskheader}>{text}</h3>
-    // {showResult && <h3 className={style.taskheader}>{answer}</h3>}
-    //const num = 3000 + getRandomInt(100) // Test *************
-    //const den = 40000 + + getRandomInt(1000) // Test *************
-    //const aufgabe = `\\[\\frac{${num}}{${den}}\\]`; // Test *************
-    //console.log(aufgabe)
-    //console.log(text)
-    //const text1 = text
-
-    //console.log(aufgabeGlobal)
-
-    
-
+        /*
     const config = {
       loader: { load: ["input/asciimath"] },
       "fast-preview": {disabled: true}
     };
-    
-    return (
-      <MathJaxContext
+
+    <MathJaxContext
       version={2}
       config={config}
-      onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
->
+      onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}>
+    */
+    return (
+    <MathJaxContext>
     <div className="Task">
-
         <h3 className={headerClassName || style.taskheader}><MathJax inline dynamic>{text}</MathJax></h3>
-        
         {showHelp && <MathJax inline dynamic><div className={style.tasktext} dangerouslySetInnerHTML={{ __html: help }} /></MathJax>}
         {showResult && <h3 className={headerClassName || style.taskheader}><MathJax inline dynamic>{answer}</MathJax></h3>}
         {showExplainer && <div  className={style.tasktext} dangerouslySetInnerHTML={{ __html: explainer }} />}
